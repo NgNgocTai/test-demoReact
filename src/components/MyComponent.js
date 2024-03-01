8e9a115aacb49ed0d92c3d6f8e7d7122b2f49839
Nguyen Ngoc Tai <23020701@vnu.edu.vn>
2024-03-01 19:14:31 +0700

    b20

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -7,14 +7,21 @@ class MyComponent extends React.Component {
     address: 'Hanoi',
     age: 19
   };
-
+  handleClick = (event) => {
+    console.log(">>>>> Click me");
+    console.log("random: ", Math.floor((Math.random()) * 100) + 1)
+    this.setState({
+      name: 'HuongGiangIdol',
+      age: Math.floor((Math.random()) * 100) + 1
+    })
+  }
   //JSX   
   render() {
     return (
       <div>
-        My name is {this.state.name}, I'm from {this.state.address}
-
-      </div>
+        My name is {this.state.name}, I'm  {this.state.age}
+        <button onClick={this.handleClick}>Click me</button>
+      </div >
 
 
     )
